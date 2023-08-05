8dd8211f72012b90661058a4d2d3644720a0eece
Roggers Anguzu <141458053+RoggersAnguzu@users.noreply.github.com>
2023-08-05 18:56:02 +0300

    Add files via upload
    Simple factorial

---
+++ FACTORIAL/FACTORIAL/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Program
+{
+    public class Recursion_Formats
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number whose Factorial is to be found: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine($"The Factorial of the number {number} is {Factorial(number)}");
+        }
+        public static int Factorial(int number)
+        {
+            if(number==0 ||number==1)
+            {
+                return 1;
+            }
+            else
+            {
+                return number * Factorial(number-1);
+            }
+        }
+    }
+}
